29b08d7a7ed4078d2c97d99eed8b91cc0081736d
cindytran <cindyztran@gmail.com>
2021-07-06 14:42:57 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+//import components
+import Header from './components/Header';
+import Main from './components/Main';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+      
+    </div>
+  );
+}
+
+export default App;
