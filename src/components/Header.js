006f5f52e969e627032498dec3f91734f7550e43
cindytran <cindyztran@gmail.com>
2021-07-06 17:50:55 -0700

    adds nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,13 @@
+import { Link } from "react-router-dom";
+
 function Header(props) {
-    return <h1>Header</h1>;
+    return (
+        <nav className="top-nav">
+            <Link to="/">
+                <div>On Tap</div>
+            </Link>
+        </nav>
+    );
 }
 
 export default Header;
