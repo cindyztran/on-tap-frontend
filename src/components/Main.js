c4ea58272248e3f31e3bfeff51b16689df2f0b8d
cindytran <cindyztran@gmail.com>
2021-07-06 18:47:02 -0700

    displays beers in index

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,49 @@
+import { useState, useEffect } from "react";
 import { Switch, Route } from "react-router-dom";
 import Index from '../pages/Index';
 import Show from '../pages/Show';
 
 function Main(props) {
+    //initializes beers state 
+    const [ beers, setBeers ] = useState(null); //nullifies initial value 
+
+    //heroku backend
+    const URL = 'https://on-tap-backend.herokuapp.com/beers/';
+
+    //fetch beers data from heroku backend
+    const getBeers = async () => {
+        const response = await fetch(URL);
+        const data = await response.json();
+        setBeers(data);
+    }
+
+    //create beers using fetch
+    const createBeers = async (beer) => {
+        //post request to create beers
+        await fetch(URL, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'Application/json'
+            },
+            body: JSON.stringify(beer)
+        });
+        //updates list of beers
+        getBeers();
+    };
+
+    useEffect(() => getBeers(), []);
+
+
+
+
+
     return (
         <main>
             <Switch>
-                <Route exact path="/">
-                    <Index />
+                <Route exact path='/'>
+                    <Index beers={beers} createBeers={createBeers}/>
                 </Route>
-                <Route path="/beers/:id" render={(rp) => (
+                <Route path='/beers/:id' render={(rp) => (
                     <Show  {...rp}/>
                 )} />
                     
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,5 +1,30 @@
+import { Link } from 'react-router-dom';
+
 function Index(props) {
-    return <h1>Index</h1>;
+
+    const loaded = () => {
+        return props.beers.map((beer) => (
+            <div key={beer._id} className='beer'>
+                <Link to={`/beers/${beer._id}`}>
+                    <h1>{beer.name}</h1>
+                </Link>
+                { beer.image && <img src={beer.image} alt={beer.name} />}
+                <section>
+                    <h3>{beer.caption}</h3>
+                    <h3>{beer.rating}</h3>
+                    <h4>{beer.city}, {beer.state} at {beer.brewery} brewery</h4>
+
+                </section>
+            </div>
+        ));
+
+    };
+
+    const loading = () => {
+        return <h1>Waiting for beer to pour...</h1>
+    };
+
+    return props.beers ? loaded() : loading();
 }
 
 export default Index;
