3559969211f2cc80aa224fd7ea00bdbf236404d7
cindytran <cindyztran@gmail.com>
2021-07-06 17:38:29 -0700

    sets up routes

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,21 @@
+import { Switch, Route } from "react-router-dom";
+import Index from '../pages/Index';
+import Show from '../pages/Show';
+
 function Main(props) {
-    return <h1>Main</h1>;
+    return (
+        <main>
+            <Switch>
+                <Route exact path="/">
+                    <Index />
+                </Route>
+                <Route path="/beers/:id" render={(rp) => (
+                    <Show  {...rp}/>
+                )} />
+                    
+            </Switch>
+        </main>
+        );
 }
 
 export default Main;
