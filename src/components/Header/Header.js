c56f4937785ca968822770b7d387c5c39278848d
Cindy Tran <cindyztran@gmail.com>
2023-03-27 22:17:54 -0700

    fix: profile pic

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,6 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faBeerMugEmpty} from '@fortawesome/free-solid-svg-icons'
 function Header(props) {
 
+    console.log({props});
     return (
         <nav className="top-nav py-1 shadow-sm"
             style={{
@@ -27,12 +28,21 @@ function Header(props) {
                     {
                         props.user ?
                         <>
-                        <div style={{marginRight: 10, color: 'white',
-                        fontSize: 15 }}>Welcome, {props.user.displayName}</div>
-                        <img src={props.user.photoURL} alt={props.user.displayName} style={{
-                            height: 30,
-                            borderRadius: '50%',
-                            marginRight: 10,
+                        <div 
+                            style={
+                                {marginRight: 10, 
+                                    color: 'white',
+                            fontSize: 15
+                             }}
+                        >
+                            Welcome, {props.user.displayName}
+                        </div>
+                        <img src={props.user.photoURL} alt='' 
+                            referrerPolicy="no-referrer"
+                            style={{
+                                height: 30,
+                                borderRadius: '50%',
+                                marginRight: 10,
                             
                         }}/>
                         <div 
